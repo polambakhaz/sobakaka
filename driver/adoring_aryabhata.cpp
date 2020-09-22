212cddab98812ccd3d845e039e6cc15fa67d372a
Daniel Someone <dsomeone@protonmail.com>
2020-09-22 22:04:40 +0000

    grmbl

---
+++ driver/adoring_aryabhata.cpp
@@ -0,0 +1,158 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.3.0
+ * Git version: 30dccd7
+ * Options:   -o driver/adoring_aryabhata.cpp
+ * Seed:      2973537605
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_8 = 0x9E53E58BL;
+static int64_t g_11 = 0x62DAAB72B21A35CALL;
+static int32_t g_13 = (-1L);
+static uint32_t g_21 = 0x29C8DDACL;
+static int8_t g_25 = (-6L);
+static int8_t g_28 = 0xBCL;
+static int32_t * const  volatile g_29 = &g_13;/* VOLATILE GLOBAL g_29 */
+
+
+/* --- FORWARD DECLARATIONS --- */
+static uint32_t  func_1(void);
+static int8_t  func_2(int32_t  p_3, int32_t  p_4, int32_t  p_5, int64_t  p_6, uint32_t  p_7);
+
+
+/* --- FUNCTIONS --- */
+/* ------------------------------------------ */
+/* 
+ * reads : g_8 g_21 g_25 g_29
+ * writes: g_11 g_21 g_25 g_28 g_13
+ */
+static uint32_t  func_1(void)
+{ /* block id: 0 */
+    uint8_t l_9 = 5UL;
+    int64_t *l_10 = &g_11;
+    int8_t *l_24 = &g_25;
+    int8_t *l_26 = (void*)0;
+    int8_t *l_27 = &g_28;
+    (*g_29) = (((((*l_27) = ((*l_24) ^= func_2(g_8, l_9, l_9, ((*l_10) = g_8), (l_9 >= g_8)))) && 0x78L) , ((l_9 || g_8) | 8L)) > (-3L));
+    return g_21;
+}
+
+
+/* ------------------------------------------ */
+/* 
+ * reads : g_21
+ * writes: g_21
+ */
+static int8_t  func_2(int32_t  p_3, int32_t  p_4, int32_t  p_5, int64_t  p_6, uint32_t  p_7)
+{ /* block id: 2 */
+    int32_t *l_12 = &g_13;
+    int32_t *l_14 = &g_13;
+    int32_t *l_15 = &g_13;
+    int32_t *l_16 = &g_13;
+    int32_t l_17 = 5L;
+    int32_t *l_18 = &l_17;
+    int32_t *l_19 = &l_17;
+    int32_t *l_20[8] = {&g_13,&g_13,&g_13,&g_13,&g_13,&g_13,&g_13,&g_13};
+    int i;
+    --g_21;
+    return p_6;
+}
+
+
+
+
+/* ---------------------------------------- */
+int main (int argc, char* argv[])
+{
+    int print_hash_value = 0;
+    if (argc == 2 && strcmp(argv[1], "1") == 0) print_hash_value = 1;
+    platform_main_begin();
+    crc32_gentab();
+    func_1();
+    transparent_crc(g_8, "g_8", print_hash_value);
+    transparent_crc(g_11, "g_11", print_hash_value);
+    transparent_crc(g_13, "g_13", print_hash_value);
+    transparent_crc(g_21, "g_21", print_hash_value);
+    transparent_crc(g_25, "g_25", print_hash_value);
+    transparent_crc(g_28, "g_28", print_hash_value);
+    platform_main_end(crc32_context ^ 0xFFFFFFFFUL, print_hash_value);
+    return 0;
+}
+
+/************************ statistics *************************
+XXX max struct depth: 0
+breakdown:
+   depth: 0, occurrence: 4
+XXX total union variables: 0
+
+XXX non-zero bitfields defined in structs: 0
+XXX zero bitfields defined in structs: 0
+XXX const bitfields defined in structs: 0
+XXX volatile bitfields defined in structs: 0
+XXX structs with bitfields in the program: 0
+breakdown:
+XXX full-bitfields structs in the program: 0
+breakdown:
+XXX times a bitfields struct's address is taken: 0
+XXX times a bitfields struct on LHS: 0
+XXX times a bitfields struct on RHS: 0
+XXX times a single bitfield on LHS: 0
+XXX times a single bitfield on RHS: 0
+
+XXX max expression depth: 15
+breakdown:
+   depth: 1, occurrence: 5
+   depth: 15, occurrence: 1
+
+XXX total number of pointers: 12
+
+XXX times a variable address is taken: 12
+XXX times a pointer is dereferenced on RHS: 0
+breakdown:
+XXX times a pointer is dereferenced on LHS: 4
+breakdown:
+   depth: 1, occurrence: 4
+XXX times a pointer is compared with null: 0
+XXX times a pointer is compared with address of another variable: 0
+XXX times a pointer is compared with another pointer: 0
+XXX times a pointer is qualified to be dereferenced: 36
+
+XXX max dereference level: 1
+breakdown:
+   level: 0, occurrence: 0
+   level: 1, occurrence: 6
+XXX number of pointers point to pointers: 0
+XXX number of pointers point to scalars: 12
+XXX number of pointers point to structs: 0
+XXX percent of pointers has null in alias set: 8.33
+XXX average alias set size: 1.08
+
+XXX times a non-volatile is read: 10
+XXX times a non-volatile is write: 8
+XXX times a volatile is read: 0
+XXX    times read thru a pointer: 0
+XXX times a volatile is write: 1
+XXX    times written thru a pointer: 0
+XXX times a volatile is available for access: 0
+XXX percentage of non-volatile access: 94.7
+
+XXX forward jumps: 0
+XXX backward jumps: 0
+
+XXX stmts: 4
+XXX max block depth: 0
+breakdown:
+   depth: 0, occurrence: 4
+
+XXX percentage a fresh-made variable is used: 25
+XXX percentage an existing variable is used: 75
+********************* end of statistics **********************/
+
