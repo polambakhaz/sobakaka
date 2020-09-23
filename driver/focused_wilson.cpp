8861c907e9a1d71b277e403362c2ba683e33e4e7
Daniel Someone <dsomeone@protonmail.com>
2020-09-23 15:04:39 +0000

    tagging release w.t.f.

---
+++ driver/focused_wilson.cpp
@@ -0,0 +1,155 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.3.0
+ * Git version: 30dccd7
+ * Options:   -o driver/focused_wilson.cpp
+ * Seed:      4189666904
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_3 = 0xC2039807L;
+static volatile uint16_t g_4[8] = {0x0111L,0x0111L,0x0111L,0x0111L,0x0111L,0x0111L,0x0111L,0x0111L};
+static volatile int8_t g_8[2][7] = {{2L,2L,2L,2L,2L,2L,2L},{(-1L),1L,(-1L),1L,(-1L),1L,(-1L)}};
+static volatile int16_t g_9[8] = {(-1L),(-1L),(-1L),(-1L),(-1L),(-1L),(-1L),(-1L)};
+static volatile uint32_t g_10[9][8] = {{0xA7BF748BL,1UL,18446744073709551615UL,0x1477A14AL,0xF7B1466AL,0xE3A4C638L,0x1477A14AL,1UL},{18446744073709551615UL,0xFF74F57BL,0x5E320525L,0x93D7F285L,0xE3A4C638L,1UL,18446744073709551613UL,1UL},{0xBD188380L,0x6D9E96BBL,4UL,0x6D9E96BBL,0xBD188380L,1UL,1UL,18446744073709551615UL},{0xD2C298C2L,0xA7BF748BL,18446744073709551615UL,0xB5CFDA6CL,1UL,18446744073709551615UL,5UL,0x6D9E96BBL},{0xFF74F57BL,0xCE3D9786L,18446744073709551615UL,0xE3A4C638L,0x3A60813CL,0xEBEF9F3BL,1UL,18446744073709551615UL},{1UL,0x1477A14AL,4UL,18446744073709551615UL,0xE3A4C638L,18446744073709551613UL,18446744073709551609UL,18446744073709551609UL},{18446744073709551615UL,0x5E320525L,0xC75DAA08L,0xC75DAA08L,0x5E320525L,18446744073709551615UL,18446744073709551615UL,1UL},{18446744073709551609UL,5UL,0xD2C298C2L,0x5E320525L,18446744073709551615UL,0xCE3D9786L,0x6D9E96BBL,5UL},{1UL,0x6D9E96BBL,18446744073709551613UL,0x5E320525L,1UL,1UL,18446744073709551615UL,1UL}};
+
+
+/* --- FORWARD DECLARATIONS --- */
+static int32_t  func_1(void);
+
+
+/* --- FUNCTIONS --- */
+/* ------------------------------------------ */
+/* 
+ * reads : g_4 g_10 g_9
+ * writes: g_4 g_10
+ */
+static int32_t  func_1(void)
+{ /* block id: 0 */
+    int32_t *l_2[5] = {&g_3,&g_3,&g_3,&g_3,&g_3};
+    int16_t l_7[2];
+    int i;
+    for (i = 0; i < 2; i++)
+        l_7[i] = 0xA0D4L;
+    ++g_4[3];
+    --g_10[0][7];
+    return g_9[1];
+}
+
+
+
+
+/* ---------------------------------------- */
+int main (int argc, char* argv[])
+{
+    int i, j;
+    int print_hash_value = 0;
+    if (argc == 2 && strcmp(argv[1], "1") == 0) print_hash_value = 1;
+    platform_main_begin();
+    crc32_gentab();
+    func_1();
+    transparent_crc(g_3, "g_3", print_hash_value);
+    for (i = 0; i < 8; i++)
+    {
+        transparent_crc(g_4[i], "g_4[i]", print_hash_value);
+        if (print_hash_value) printf("index = [%d]\n", i);
+
+    }
+    for (i = 0; i < 2; i++)
+    {
+        for (j = 0; j < 7; j++)
+        {
+            transparent_crc(g_8[i][j], "g_8[i][j]", print_hash_value);
+            if (print_hash_value) printf("index = [%d][%d]\n", i, j);
+
+        }
+    }
+    for (i = 0; i < 8; i++)
+    {
+        transparent_crc(g_9[i], "g_9[i]", print_hash_value);
+        if (print_hash_value) printf("index = [%d]\n", i);
+
+    }
+    for (i = 0; i < 9; i++)
+    {
+        for (j = 0; j < 8; j++)
+        {
+            transparent_crc(g_10[i][j], "g_10[i][j]", print_hash_value);
+            if (print_hash_value) printf("index = [%d][%d]\n", i, j);
+
+        }
+    }
+    platform_main_end(crc32_context ^ 0xFFFFFFFFUL, print_hash_value);
+    return 0;
+}
+
+/************************ statistics *************************
+XXX max struct depth: 0
+breakdown:
+   depth: 0, occurrence: 5
+XXX total union variables: 0
+
+XXX non-zero bitfields defined in structs: 0
+XXX zero bitfields defined in structs: 0
+XXX const bitfields defined in structs: 0
+XXX volatile bitfields defined in structs: 0
+XXX structs with bitfields in the program: 0
+breakdown:
+XXX full-bitfields structs in the program: 0
+breakdown:
+XXX times a bitfields struct's address is taken: 0
+XXX times a bitfields struct on LHS: 0
+XXX times a bitfields struct on RHS: 0
+XXX times a single bitfield on LHS: 0
+XXX times a single bitfield on RHS: 0
+
+XXX max expression depth: 1
+breakdown:
+   depth: 1, occurrence: 5
+
+XXX total number of pointers: 1
+
+XXX times a variable address is taken: 1
+XXX times a pointer is dereferenced on RHS: 0
+breakdown:
+XXX times a pointer is dereferenced on LHS: 0
+breakdown:
+XXX times a pointer is compared with null: 0
+XXX times a pointer is compared with address of another variable: 0
+XXX times a pointer is compared with another pointer: 0
+XXX times a pointer is qualified to be dereferenced: 0
+XXX number of pointers point to pointers: 0
+XXX number of pointers point to scalars: 1
+XXX number of pointers point to structs: 0
+XXX percent of pointers has null in alias set: 100
+XXX average alias set size: 2
+
+XXX times a non-volatile is read: 0
+XXX times a non-volatile is write: 0
+XXX times a volatile is read: 1
+XXX    times read thru a pointer: 0
+XXX times a volatile is write: 2
+XXX    times written thru a pointer: 0
+XXX times a volatile is available for access: 5
+XXX percentage of non-volatile access: 0
+
+XXX forward jumps: 0
+XXX backward jumps: 0
+
+XXX stmts: 3
+XXX max block depth: 0
+breakdown:
+   depth: 0, occurrence: 3
+
+XXX percentage a fresh-made variable is used: 41.7
+XXX percentage an existing variable is used: 58.3
+FYI: the random generator makes assumptions about the integer size. See platform.info for more details.
+********************* end of statistics **********************/
+
