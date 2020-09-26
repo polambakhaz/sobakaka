297ad4357611839c6076010be1a3bb5c236bfaea
Daniel Someone <dsomeone@protonmail.com>
2020-09-26 13:04:38 +0000

    debug line test

---
+++ driver/hopeful_jennings.cpp
@@ -0,0 +1,146 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.3.0
+ * Git version: 30dccd7
+ * Options:   -o driver/hopeful_jennings.cpp
+ * Seed:      30230252
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_2 = 0x1928C176L;
+static volatile int32_t g_5 = 0L;/* VOLATILE GLOBAL g_5 */
+static volatile int32_t g_6 = 0xC70C3099L;/* VOLATILE GLOBAL g_6 */
+static volatile int32_t g_7 = 0x04822B16L;/* VOLATILE GLOBAL g_7 */
+static volatile int32_t g_8 = 0x849F845DL;/* VOLATILE GLOBAL g_8 */
+static int32_t g_9 = 0xF07B4BC6L;
+static int32_t g_13 = 0x6310FECAL;
+
+
+/* --- FORWARD DECLARATIONS --- */
+static int32_t  func_1(void);
+
+
+/* --- FUNCTIONS --- */
+/* ------------------------------------------ */
+/* 
+ * reads : g_2 g_9 g_7
+ * writes: g_2 g_9 g_13
+ */
+static int32_t  func_1(void)
+{ /* block id: 0 */
+    int32_t l_16 = 0x2F67D50FL;
+    for (g_2 = (-10); (g_2 != (-27)); --g_2)
+    { /* block id: 3 */
+        int32_t *l_15 = &g_2;
+        int32_t **l_14 = &l_15;
+        for (g_9 = 0; (g_9 > 24); g_9++)
+        { /* block id: 6 */
+            int32_t *l_12 = &g_13;
+            (*l_12) = 0xC3938DD4L;
+            l_14 = (void*)0;
+        }
+        return l_16;
+    }
+    return g_7;
+}
+
+
+
+
+/* ---------------------------------------- */
+int main (int argc, char* argv[])
+{
+    int print_hash_value = 0;
+    if (argc == 2 && strcmp(argv[1], "1") == 0) print_hash_value = 1;
+    platform_main_begin();
+    crc32_gentab();
+    func_1();
+    transparent_crc(g_2, "g_2", print_hash_value);
+    transparent_crc(g_5, "g_5", print_hash_value);
+    transparent_crc(g_6, "g_6", print_hash_value);
+    transparent_crc(g_7, "g_7", print_hash_value);
+    transparent_crc(g_8, "g_8", print_hash_value);
+    transparent_crc(g_9, "g_9", print_hash_value);
+    transparent_crc(g_13, "g_13", print_hash_value);
+    platform_main_end(crc32_context ^ 0xFFFFFFFFUL, print_hash_value);
+    return 0;
+}
+
+/************************ statistics *************************
+XXX max struct depth: 0
+breakdown:
+   depth: 0, occurrence: 2
+XXX total union variables: 0
+
+XXX non-zero bitfields defined in structs: 0
+XXX zero bitfields defined in structs: 0
+XXX const bitfields defined in structs: 0
+XXX volatile bitfields defined in structs: 0
+XXX structs with bitfields in the program: 0
+breakdown:
+XXX full-bitfields structs in the program: 0
+breakdown:
+XXX times a bitfields struct's address is taken: 0
+XXX times a bitfields struct on LHS: 0
+XXX times a bitfields struct on RHS: 0
+XXX times a single bitfield on LHS: 0
+XXX times a single bitfield on RHS: 0
+
+XXX max expression depth: 2
+breakdown:
+   depth: 1, occurrence: 6
+   depth: 2, occurrence: 2
+
+XXX total number of pointers: 3
+
+XXX times a variable address is taken: 3
+XXX times a pointer is dereferenced on RHS: 0
+breakdown:
+XXX times a pointer is dereferenced on LHS: 1
+breakdown:
+   depth: 1, occurrence: 1
+XXX times a pointer is compared with null: 0
+XXX times a pointer is compared with address of another variable: 0
+XXX times a pointer is compared with another pointer: 0
+XXX times a pointer is qualified to be dereferenced: 0
+
+XXX max dereference level: 1
+breakdown:
+   level: 0, occurrence: 0
+   level: 1, occurrence: 2
+XXX number of pointers point to pointers: 1
+XXX number of pointers point to scalars: 2
+XXX number of pointers point to structs: 0
+XXX percent of pointers has null in alias set: 33.3
+XXX average alias set size: 1.33
+
+XXX times a non-volatile is read: 4
+XXX times a non-volatile is write: 5
+XXX times a volatile is read: 1
+XXX    times read thru a pointer: 0
+XXX times a volatile is write: 0
+XXX    times written thru a pointer: 0
+XXX times a volatile is available for access: 5
+XXX percentage of non-volatile access: 90
+
+XXX forward jumps: 0
+XXX backward jumps: 0
+
+XXX stmts: 6
+XXX max block depth: 2
+breakdown:
+   depth: 0, occurrence: 2
+   depth: 1, occurrence: 2
+   depth: 2, occurrence: 2
+
+XXX percentage a fresh-made variable is used: 66.7
+XXX percentage an existing variable is used: 33.3
+********************* end of statistics **********************/
+
