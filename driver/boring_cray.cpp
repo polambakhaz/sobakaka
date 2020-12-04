8bddfab501aff0765c7e9494e4fc326e628558dc
Daniel Someone <dsomeone@protonmail.com>
2020-12-04 12:04:40 +0000

    clarify further the brokenness of C++. why the fuck are we using C++?

---
+++ driver/boring_cray.cpp
@@ -0,0 +1,131 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.3.0
+ * Git version: 30dccd7
+ * Options:   -o driver/boring_cray.cpp
+ * Seed:      4269124075
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static volatile uint32_t g_2[9] = {0x81A7ECC2L,0x81A7ECC2L,0x81A7ECC2L,0x81A7ECC2L,0x81A7ECC2L,0x81A7ECC2L,0x81A7ECC2L,0x81A7ECC2L,0x81A7ECC2L};
+static int32_t g_4 = 0xDA2282D5L;
+
+
+/* --- FORWARD DECLARATIONS --- */
+static uint8_t  func_1(void);
+
+
+/* --- FUNCTIONS --- */
+/* ------------------------------------------ */
+/* 
+ * reads : g_2 g_4
+ * writes: g_4
+ */
+static uint8_t  func_1(void)
+{ /* block id: 0 */
+    int32_t *l_3 = &g_4;
+    uint32_t l_5[10][6][2] = {{{4294967294UL,0UL},{4294967289UL,4294967287UL},{0xE72EE72CL,0xABFD090DL},{0xB0AD8EA5L,0x77CCE45DL},{0xFACD4A79L,0xDDF34FFDL},{4294967295UL,1UL}},{{0xC9FE9A5AL,0xB0AD8EA5L},{0xB30498D2L,4294967295UL},{0xDDF34FFDL,0UL},{0xE72EE72CL,4294967295UL},{1UL,4294967294UL},{0UL,0x4CCBB24AL}},{{0xABFD090DL,1UL},{2UL,1UL},{4294967295UL,0xB30498D2L},{0x4CCBB24AL,0xF2EF51AEL},{0xE72EE72CL,0xF2EF51AEL},{0x4CCBB24AL,0xB30498D2L}},{{4294967295UL,0x5AD33D2AL},{1UL,0xEDAD94E8L},{0xBEBF0A0CL,4294967295UL},{0UL,4294967295UL},{0x5AD33D2AL,0x4CCBB24AL},{0xD96753F3L,0xDDF34FFDL}},{{1UL,0UL},{0x4694486CL,0xE72EE72CL},{4294967289UL,0xEDAD94E8L},{0x4CCBB24AL,1UL},{0UL,4294967290UL},{0xE72EE72CL,0xBEBF0A0CL}},{{0xD96753F3L,0xB0AD8EA5L},{0x0CC321A0L,0UL},{4294967295UL,0xD8FF8CD3L},{0xB0AD8EA5L,0xEDAD94E8L},{0x83518F4CL,0x0CC321A0L},{4294967293UL,4294967293UL}},{{0xD8FF8CD3L,1UL},{0xD96753F3L,4294967289UL},{0xE7528ACEL,0UL},{4294967290UL,0xE7528ACEL},{0xDDF34FFDL,0xEDAD94E8L},{0xDDF34FFDL,0xE7528ACEL}},{{4294967290UL,0UL},{0xE7528ACEL,4294967289UL},{0xD96753F3L,1UL},{0xD8FF8CD3L,4294967293UL},{4294967293UL,0x0CC321A0L},{0x83518F4CL,0xEDAD94E8L}},{{0xB0AD8EA5L,0xD8FF8CD3L},{4294967295UL,0UL},{0x0CC321A0L,0xB0AD8EA5L},{0xD96753F3L,0xBEBF0A0CL},{0xE72EE72CL,4294967290UL},{0UL,1UL}},{{0x4CCBB24AL,0xEDAD94E8L},{4294967289UL,0xE72EE72CL},{0x4694486CL,0UL},{1UL,0xDDF34FFDL},{0xD96753F3L,0x4CCBB24AL},{0x5AD33D2AL,4294967295UL}}};
+    int i, j, k;
+    (*l_3) ^= g_2[8];
+    return l_5[7][1][1];
+}
+
+
+
+
+/* ---------------------------------------- */
+int main (int argc, char* argv[])
+{
+    int i;
+    int print_hash_value = 0;
+    if (argc == 2 && strcmp(argv[1], "1") == 0) print_hash_value = 1;
+    platform_main_begin();
+    crc32_gentab();
+    func_1();
+    for (i = 0; i < 9; i++)
+    {
+        transparent_crc(g_2[i], "g_2[i]", print_hash_value);
+        if (print_hash_value) printf("index = [%d]\n", i);
+
+    }
+    transparent_crc(g_4, "g_4", print_hash_value);
+    platform_main_end(crc32_context ^ 0xFFFFFFFFUL, print_hash_value);
+    return 0;
+}
+
+/************************ statistics *************************
+XXX max struct depth: 0
+breakdown:
+   depth: 0, occurrence: 2
+XXX total union variables: 0
+
+XXX non-zero bitfields defined in structs: 0
+XXX zero bitfields defined in structs: 0
+XXX const bitfields defined in structs: 0
+XXX volatile bitfields defined in structs: 0
+XXX structs with bitfields in the program: 0
+breakdown:
+XXX full-bitfields structs in the program: 0
+breakdown:
+XXX times a bitfields struct's address is taken: 0
+XXX times a bitfields struct on LHS: 0
+XXX times a bitfields struct on RHS: 0
+XXX times a single bitfield on LHS: 0
+XXX times a single bitfield on RHS: 0
+
+XXX max expression depth: 1
+breakdown:
+   depth: 1, occurrence: 3
+
+XXX total number of pointers: 1
+
+XXX times a variable address is taken: 1
+XXX times a pointer is dereferenced on RHS: 0
+breakdown:
+XXX times a pointer is dereferenced on LHS: 1
+breakdown:
+   depth: 1, occurrence: 1
+XXX times a pointer is compared with null: 0
+XXX times a pointer is compared with address of another variable: 0
+XXX times a pointer is compared with another pointer: 0
+XXX times a pointer is qualified to be dereferenced: 0
+
+XXX max dereference level: 1
+breakdown:
+   level: 0, occurrence: 0
+   level: 1, occurrence: 2
+XXX number of pointers point to pointers: 0
+XXX number of pointers point to scalars: 1
+XXX number of pointers point to structs: 0
+XXX percent of pointers has null in alias set: 0
+XXX average alias set size: 1
+
+XXX times a non-volatile is read: 1
+XXX times a non-volatile is write: 2
+XXX times a volatile is read: 1
+XXX    times read thru a pointer: 0
+XXX times a volatile is write: 0
+XXX    times written thru a pointer: 0
+XXX times a volatile is available for access: 0
+XXX percentage of non-volatile access: 75
+
+XXX forward jumps: 0
+XXX backward jumps: 0
+
+XXX stmts: 2
+XXX max block depth: 0
+breakdown:
+   depth: 0, occurrence: 2
+
+XXX percentage a fresh-made variable is used: 100
+XXX percentage an existing variable is used: 0
+FYI: the random generator makes assumptions about the integer size. See platform.info for more details.
+********************* end of statistics **********************/
+
